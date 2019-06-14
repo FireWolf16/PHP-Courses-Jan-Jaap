d1aab01960caa55fb5b96379564ebedc4c06503c
patrick <patrick.laranjo@gmail.com>
2019-06-14 08:52:58 +0200

    1.2

--- 100 php mix met html.php
+++ 100 php mix met html.php
@@ -1,7 +1,6 @@
 <?php
 
 ?>
-
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,8 +9,31 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style>
+        img{
+            height: 200px;
+            width: 200px;
+        }
+    </style>
 </head>
 <body>
-<p><?php echo "testje"; ?></p>
+<?php
+/*for loop*/
+//for ($i = 1; $i<=10; $i++) {
+//    echo "<img src='img/MK-characters".$i.".png'>";
+//}
+
+/**/
+//$nextC = array("330px-BarakaMK11.png", "KitanaMK11.png", "Noob_Saibot.png", "Shao_Kahn_2011.png");
+//foreach ($nextC as $next) {
+//    echo "<img src='img/next-MK-character/".$next."'>";
+//}
+
+$i= 1;
+while ($i<=10) {
+    echo "<img src='img/MK-characters".$i.".png'>";
+    $i++;
+}
+?>
 </body>
 </html>
